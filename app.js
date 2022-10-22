1a903e52f54be4221801198e56fc61a7f4bbe226
Omer Evron <omerevron011@gmail.com>
2022-10-23 01:08:56 +0300

    inventory&rocks

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const start = document.getElementById('start') 
+const startContainer = document.querySelector('.startContainer') 
+start.addEventListener('click', (e)=>{
+    startContainer.classList.add('hidden')
+    main()
+    
+})
+
 function main() {
-    const overlayBp = document.getElementById('overlay')
-    const size = 16
     const closeBtn = document.getElementById('close')
+    const reset = document.getElementById('reset')
     const grid = document.getElementById('gameBoard')
-    const axe = document.getElementById('axe')
-    const pickaxe = document.getElementById('pickaxe')
-    const shovel = document.getElementById('shovel')
-    let selectedTool = ''
-    const docInventory = document.getElementById('inventory')
     const inventory = {
         grass: 0,
         leaves: 0,
@@ -15,6 +17,14 @@ function main() {
         ground: 0,
         rock: 0,
     }
+    const overlayBp = document.getElementById('overlay')
+    const backpack =  document.getElementById('backpack').children
+    const size = 16
+    const axe = document.getElementById('axe')
+    const pickaxe = document.getElementById('pickaxe')
+    const shovel = document.getElementById('shovel')
+    let selectedTool = ''
+    const docInventory = document.getElementById('inventory')
     //Tools ------------------------------------
     axe.addEventListener('click', (e) => {
         selectedTool = 'axe'
@@ -44,15 +54,15 @@ function main() {
     element.setAttribute("class", "grid-item");
     grid.appendChild(element);
     // Tools Funcionality-----------------------------------
-    element.addEventListener("click", (e) => {
-        if (selectedTool === 'shovel') {
-            element.classList.remove('ground', 'grass')
-        } else if (selectedTool === 'pickaxe') {
-            element.classList.remove('rock')
-        } else if (selectedTool === 'axe') {
-            element.classList.remove('tree', 'leaves')
-        }
-    });
+    // element.addEventListener("click", (e) => {
+    //     if (selectedTool === 'shovel') {
+    //         element.classList.remove('ground', 'grass')
+    //     } else if (selectedTool === 'pickaxe') {
+    //         element.classList.remove('rock')
+    //     } else if (selectedTool === 'axe') {
+    //         element.classList.remove('tree', 'leaves')
+    //     }
+    // });
     }
     }
     }
@@ -63,10 +73,17 @@ function main() {
     function randomNumHelper() {
         return Math.floor(Math.random() * size)
     }
+    function selected(el) {
+       
+        el.classList.add('selected')
+            // el.nextElementSibling.classList.remove('selected')
+            // el.previousElementSibling.classList.remove('selected')
+        
+    }
 
     //Board Elements-----------------------------------
+    const gridItems = document.querySelectorAll('.grid-item')
     function fillCells() {
-        const gridItems = document.querySelectorAll('.grid-item')
         for (let k = 0; k < gridItems.length; k++) {
             let [i, j] = gridItems[k].getAttribute('id').slice(2).split('-')
             fillGround(k, i)
@@ -113,7 +130,7 @@ function main() {
         function fillRocks(k, i, j) {
             for (let i = 0; i < size; i++) {
                 let random = randomNumHelper()
-                if (j == random && i > 12)
+                if (j == random && i > 11)
                     gridItems[k].classList.add('rock')
             }
         }
@@ -121,41 +138,88 @@ function main() {
     fillCells()
 
     // Inventory------------------------------------------------------
-    // function updateDocInventory() {
-    //     docInventory.innerText++
-    // }
+    let selectedFromInventory = ''
+    function updateDocInventory() {
+     for (let element of backpack) {
+        if (element.classList.contains('rock')) {
+            element.innerText = `${inventory.rock}`
+        }
+        else if (element.classList.contains('ground')) {
+            element.innerText = `${inventory.ground}`
+        }
+        else if (element.classList.contains('grass')) {
+            element.innerText = `${inventory.grass}`
+        }
+        else if (element.classList.contains('tree')) {
+            element.innerText = `${inventory.tree}`
+        }
+        else if (element.classList.contains('leaves')) {
+            element.innerText = `${inventory.leaves}`
+        }
+     }
+    }
+    
     grid.addEventListener('click', e=>{
-        if(e.target.classList.contains('rock')){ 
+        if(selectedFromInventory.length>0 && selectedTool===''&& inventory[selectedFromInventory]>0){
+            e.target.classList.add(selectedFromInventory)
+            inventory[selectedFromInventory]--
+            updateDocInventory()
+        }
+        if(e.target.classList.contains('rock') && selectedTool =='pickaxe'){ 
+            e.target.classList.remove('rock')
             inventory.rock++
-            // updateDocInventory()
+            updateDocInventory()
         }
-        else if(e.target.classList.contains('grass')){ 
+        else if(e.target.classList.contains('grass') && selectedTool =='shovel'){ 
+            e.target.classList.remove('grass')
             inventory.grass++
-            // updateDocInventory()
+            updateDocInventory()
         }
-        else if(e.target.classList.contains('ground')){ 
+        else if(e.target.classList.contains('ground') && selectedTool =='shovel'){ 
+            e.target.classList.remove('ground')
             inventory.ground++
-            // updateDocInventory()
+            updateDocInventory()
         }
-        else if(e.target.classList.contains('leaves')){ 
+        else if(e.target.classList.contains('leaves') && selectedTool =='axe' ){ 
+            e.target.classList.remove('leaves')
             inventory.leaves++
-            // updateDocInventory()
+            updateDocInventory()
         }
-        else if(e.target.classList.contains('tree')){ 
+        else if(e.target.classList.contains('tree')&& selectedTool =='axe'){ 
+            e.target.classList.remove('tree')
             inventory.tree++ 
-            // updateDocInventory()
+            updateDocInventory()
         }
-        // BackPack------------------------------------------------------
-        closeBtn.addEventListener('click', ()=>{
-            overlayBp.classList.toggle('hidden')
-        })
         
+    })
+    // BackPack------------------------------------------------------
+    closeBtn.addEventListener('click', ()=>{
+        overlayBp.classList.toggle('hidden')
     })
     docInventory.addEventListener('click', ()=>{
         overlayBp.classList.remove('hidden')
     })
     
+    function backpackSelect() {
+        for (let el of backpack) {
+            el.addEventListener('click',()=>{
+                overlayBp.classList.toggle('hidden')
+                selectedFromInventory = el.getAttribute('class')
+                selectedTool=''
+            })}
+    }
+    backpackSelect()
+    reset.addEventListener('click', ()=>{
+    grid.childNodes.forEach(element => {
+        element.classList.remove('rock','tree','ground','grass','leaves')
+    });
+    // inventory.grass =0
+    // inventory.leaves =0
+    // inventory.tree =0
+    // inventory.rock =0
+    // inventory.ground =0
+    // updateDocInventory()
+        main()
+    })
     
 }
-
-main();
